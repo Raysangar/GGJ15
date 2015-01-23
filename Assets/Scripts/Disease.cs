89319fd70c317af55a698c73ac2ab9a3b50613f9
Ivan <eforetholagar@gmail.com>
2015-01-23 21:18:02 +0100

    heal method implemented

--- Assets/Scripts/Disease.cs
+++ Assets/Scripts/Disease.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Desease : MonoBehaviour {
+
+    public ArrayList medicineList;
 
 	// Use this for initialization
 	void Start () {
-	
+        medicineList = new ArrayList();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public int heal(ArrayList medicines)
+    {
+        int works = 0;
+        foreach (var medicine in medicines)
+        {
+            if (medicineList.Contains(medicine))
+                ++works;
+            else
+                --works;
+        }
+        return works;
+    }
 }

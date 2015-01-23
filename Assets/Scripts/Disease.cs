9840af4842481f1740e3049028a789f4c3e607bc
Rayco Sánchez García <raycosanchezgarcia@gmail.com>
2015-01-23 21:42:06 +0100

    Patient component added

--- Assets/Scripts/Disease.cs
+++ Assets/Scripts/Disease.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Desease : MonoBehaviour {
+public class Disease : MonoBehaviour {
 
     public ArrayList medicineList;
 
@@ -10,17 +10,7 @@ public class Desease : MonoBehaviour {
 	void Start () {
         medicineList = new ArrayList();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
-<<<<<<< HEAD
-	void heal(ArrayList medicines)  {
-		
-	}
-=======
     public int heal(ArrayList medicines)
     {
         int works = 0;
@@ -33,5 +23,4 @@ public class Desease : MonoBehaviour {
         }
         return works;
     }
->>>>>>> 89319fd70c317af55a698c73ac2ab9a3b50613f9
 }

fe42dd93cfe938b55393010f9ef9793b46c553e9
Ivan <eforetholagar@gmail.com>
2015-01-24 11:43:29 +0100

    Patient and game controller modified

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,22 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class GameController : MonoBehaviour {
-	GameObject patient;
+	public GameObject patient;
 	public float lifeTimePenalty;
+    public Camera mainCamera;
+
+    private int patientNum;
+ 
 	// Use this for initialization
 	void Start () {
-	
+        patientNum = 0;
 	}
+
+    int getPatientNum()
+    {
+        return patientNum;
+    }
 
 	void heal (ArrayList medicines) {
 		int diseaseHealed = patient.GetComponent<Patient> ().heal (medicines);
 		if (diseaseHealed < 0)
 			patient.GetComponent<LifeTimer> ().lifeTime -= lifeTimePenalty;
-		else if (diseaseHealed > 0 && patient.GetComponent<Patient> ().isHealthy ())
-			nextPatient ();
+        else if (diseaseHealed > 0 && patient.GetComponent<Patient>().isHealthy())
+        {
+            mainCamera.GetComponent<CameraController>().BriefRotation();
+            nextPatient();
+        }
 	}
 
 	private void nextPatient() {
+        patient.GetComponent<Patient>().createPatient(patientNum);
 	}
 }
